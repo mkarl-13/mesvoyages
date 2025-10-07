349ea19892db9c97ddb3885f614226f2382612ab
Karl <Karl@DESKTOP-9EE2SD4>
2025-10-08 00:01:58 +0200

    Phase 10 : ORM (affichage détaillé d'une visite).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -54,4 +54,12 @@ class VoyagesController extends AbstractController {
            "visites" => $visites 
         ]);
     }
+    
+    #[Route("/voyages/voyage/{id}", name: "voyages.showone")]
+    public function showOne($id): Response{
+        $visite = $this->repository->find($id);
+        return $this->render("pages/voyage.html.twig", [
+            "visite" => $visite
+        ]);
+    }
 }
