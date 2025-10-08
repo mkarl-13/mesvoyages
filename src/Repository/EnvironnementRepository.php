f2329a58059155b22dfe7e661e736b5aee24840a
Karl <Karl@DESKTOP-9EE2SD4>
2025-10-08 19:41:14 +0200

    Phase 15 : ORM (environnements : ajout et suppression).

--- src/Repository/EnvironnementRepository.php
+++ src/Repository/EnvironnementRepository.php
@@ -9,10 +9,9 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<Environnement>
  */
-class EnvironnementRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
+class EnvironnementRepository extends ServiceEntityRepository {
+
+    public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, Environnement::class);
     }
 
@@ -30,7 +29,6 @@ class EnvironnementRepository extends ServiceEntityRepository
     //            ->getResult()
     //        ;
     //    }
-
     //    public function findOneBySomeField($value): ?Environnement
     //    {
     //        return $this->createQueryBuilder('e')
@@ -40,4 +38,24 @@ class EnvironnementRepository extends ServiceEntityRepository
     //            ->getOneOrNullResult()
     //        ;
     //    }
+
+    /**
+     * Supprime un environnement
+     * @param Environnement $environnement
+     * @return void
+     */
+    public function remove(Environnement $environnement): void {
+        $this->getEntityManager()->remove($environnement);
+        $this->getEntityManager()->flush();
+    }
+
+    /**
+     * Ajoute un environnement
+     * @param Environnement $environnement
+     * @return void
+     */
+    public function add(Environnement $environnement): void {
+        $this->getEntityManager()->persist($environnement);
+        $this->getEntityManager()->flush();
+    }
 }
