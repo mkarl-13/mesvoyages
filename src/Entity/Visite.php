70852614df530322fd82b7ab7b5e4e6d133c5175
Karl <Karl@DESKTOP-9EE2SD4>
2025-10-08 21:26:25 +0200

    Phase 18 : gestion des images.

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -7,8 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: VisiteRepository::class)]
+#[Vich\Uploadable]
 class Visite
 {
     #[ORM\Id]
@@ -37,6 +42,18 @@ class Visite
     #[ORM\Column(nullable: true)]
     private ?int $tempmax = null;
 
+    #[Vich\UploadableField(mapping: "visites", fileNameProperty: "imageName", size: "imageSize")]
+    private ?File $imageFile = null;
+    
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+    
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
+    
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+    
     /**
      * @var Collection<int, Environnement>
      */
@@ -168,5 +185,51 @@ class Visite
         $this->environnements->removeElement($environnement);
 
         return $this;
+    }
+    
+    public function getImageFile(): ?File {
+        return $this->imageFile;
+    }
+
+    public function getImageName(): ?string {
+        return $this->imageName;
+    }
+
+    public function getImageSize(): ?int {
+        return $this->imageSize;
+    }
+
+    public function setImageFile(?File $imageFile): void {
+        $this->imageFile = $imageFile;
+        if(null !== $imageFile) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function setImageName(?string $imageName): void {
+        $this->imageName = $imageName;
+    }
+
+    public function setImageSize(?int $imageSize): void {
+        $this->imageSize = $imageSize;
+    }
+    
+    #[Assert\Callback]
+    public function validate(ExecutionContextInterface $context) {
+        $file = $this->getImageFile();
+        if($file != null && $file != "") {
+            $poids = @filesize($file);
+            if($poids != false && $poids > 512000) {
+                $context->buildViolation("Cette image est trop lourde (500Ko max)")
+                ->atPath("imageFile")
+                ->addViolation();
+            }
+            $infosImage = @getimagesize($file);
+            if ($infosImage == false) {
+                $context->buildViolation("Ce fichier n'est pas une image")
+                ->atPath("imageFile")
+                ->addViolation();
+            }
+        }
     }
 }
