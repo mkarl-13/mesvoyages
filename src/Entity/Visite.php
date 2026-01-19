512b144f86b187391d812f11d2533b61c77b052d
Karl <Karl@DESKTOP-9EE2SD4>
2026-01-19 16:52:07 +0100

    Phase 23 : tests (compléments).

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -28,6 +28,7 @@ class Visite
     private ?string $pays = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    #[Assert\LessThanOrEqual("now")]
     private ?\DateTime $datecreation = null;
 
     #[ORM\Column(nullable: true)]
--- tests/VisiteTest.php
+++ tests/VisiteTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Entity\Environnement;
 use App\Entity\Visite;
 use DateTime;
 use PHPUnit\Framework\TestCase;
@@ -17,5 +18,16 @@ class VisiteTest extends TestCase {
         $visite = new Visite();
         $visite->setDateCreation(new DateTime("2024-04-24"));
         $this->assertEquals("24/04/2024", $visite->getDatecreationString());
+    }
+    
+    public function testAddEnvironnement(){
+        $environnement = new Environnement();
+        $environnement->setNom("plage");
+        $visite = new Visite();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementAvant = $visite->getEnvironnements()->count();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementApres = $visite->getEnvironnements()->count();
+        $this->assertEquals($nbEnvironnementAvant, $nbEnvironnementApres, "ajout même environnement devrait échouer");
     }
 }
