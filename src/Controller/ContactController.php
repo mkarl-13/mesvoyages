cb604b7572c0d84a8b9f2c44cc539f34be0aef30
Karl <Karl@DESKTOP-9EE2SD4>
2026-01-20 16:54:58 +0100

    Phase 24 : déploiement.

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -40,7 +40,7 @@ class ContactController extends AbstractController {
     public function sendEmail(MailerInterface $mailer, Contact $contact) {
         $email = (new Email())
                 ->from($contact->getEmail())
-                ->to("contact@mesvoyages.com")
+                ->to("mitschikarl@gmail.com")
                 ->subject("Message du site de voyages")
                 ->html($this->renderView(
                         "pages/_email.html.twig", [
